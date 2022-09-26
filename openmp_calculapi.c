62c7ccd441d2d8b714c250fc4cdad6098a1c01ec
Viviane Romero <viviromerobr@gmail.com>
2022-09-26 08:57:14 -0300

    Update and rename calculapi_openmp.c to openmp_calculapi.c

--- openmp_calculapi.c
+++ openmp_calculapi.c
@@ -6,14 +6,14 @@ double step;
 #define NUM_THREADS 5
 
 void main (){ 
-	int i, nthreads; double x, pi, sum[NUM_THREADS];
-	sum[0] = 0.0;
+	int i, nthreads; double pi, sum[NUM_THREADS];
+	for(i=0; i<nthreads; i++) sum[i] = 0.0;
 	step = 1.0/(double) num_steps;
 	omp_set_num_threads (NUM_THREADS);
 
 	#pragma omp parallel
 	{ 
-		int i, id, nthrds;
+		int i, id, nthrds; double x;
 		id = omp_get_thread_num();
 		nthrds = omp_get_num_threads();
 		if(id == 0) nthreads = nthrds;
