481cfde24d1152bd34a08268825111562abebee8
Viviane Romero <viviromerobr@gmail.com>
2022-10-24 08:06:16 -0300

    Add files via upload

---
+++ calculapi_openmp_critical2.c
@@ -0,0 +1,31 @@
+#include <omp.h>
+#include <stdio.h>
+
+static long num_steps = 100000;
+double step;
+#define NUM_THREADS 5
+
+void main (){ 
+	int i, nthreads; double pi;
+	step = 1.0/(double) num_steps;
+	omp_set_num_threads (NUM_THREADS);
+
+	#pragma omp parallel
+	{ 
+		int i, id, nthrds; double x;
+		id = omp_get_thread_num();
+		nthrds = omp_get_num_threads();
+
+		for (i=id; i<num_steps; i=i+nthrds){
+			x = (i+0.5)*step;
+
+			#pragma omp critical
+				pi += (4.0/(1.0+x*x));
+		}
+
+	}
+
+	pi = pi * step;
+
+	printf("%f\n", pi);
+}
